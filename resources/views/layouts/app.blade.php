a01bb365594aff1292392be6a5f3e5274c39f355
unknown <fabian13sosaalva@gmail.com>
2024-11-15 03:19:11 -0500

    vista cliente Style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <!-- Agrega el archivo CSS -->
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
+    <!-- Navbar -->
     <nav>
         <div>
             <a href="{{ route('vistaprincipal') }}">INICIO</a>
@@ -16,10 +18,12 @@
         <div class="welcome">Bienvenido, Gabriel Lozano</div>
     </nav>
 
+    <!-- Contenido Principal -->
     <main>
         @yield('content')
     </main>
 
+    <!-- Footer -->
     <footer>
         <p>DAGA Soluciones Técnicas © 2024</p>
     </footer>
